9dc9775248157e077c7f186c2714960c07882c50
jhe381697 <herlemont.josephine@gmail.com>
2022-05-07 17:06:19 +0200

    react to the event

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,12 +11,24 @@ export class FaceSnapComponent implements OnInit {
   createdDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  buttonText!: string;
 
   ngOnInit() {
     this.title = 'Spiderman';
     this.description = 'A Marvel super hero';
     this.createdDate = new Date();
     this.snaps = 7;
-    this.imageUrl = 'https://images.unsplash.com/photo-1524481905007-ea072534b820?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80;'
+    this.imageUrl = 'https://images.unsplash.com/photo-1524481905007-ea072534b820?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80';
+    this.buttonText = 'Oh snap!';
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh snap!') {
+      this.snaps++;
+      this.buttonText = 'Oops, un snap!';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh snap!';
+    }
   }
 }
