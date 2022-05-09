15074d4e0888bb8fbb27356372b1a8b9d760fe9e
jhe381697 <herlemont.josephine@gmail.com>
2022-05-09 16:29:24 +0200

    *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,35 +7,33 @@ import { FaceSnap } from './models/face-snap.models';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = {
-      title:'Kylo Ren',
-      description: 'Sith',
-      imageUrl: 'https://images.unsplash.com/photo-1623476408624-721c9185d569?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'planète inconnue'
-    };
-
-    this.myOtherSnap = {
-      title: 'A planet',
-      description: 'Star wars univer',
-      imageUrl: 'https://images.unsplash.com/photo-1507499739999-097706ad8914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'espace'
-    };
-    
-    this.myLastSnap = {
-      title: 'BB-8',
-      description: 'Droïd',
-      imageUrl: 'https://images.unsplash.com/photo-1592513735373-ae19bac7fde8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
-      createdDate: new Date(),
-      snaps: 0
-    };
+    this.faceSnaps = [
+      {
+        title:'Kylo Ren',
+        description: 'Sith',
+        imageUrl: 'https://images.unsplash.com/photo-1623476408624-721c9185d569?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'planète inconnue'
+      },
+      {
+        title: 'A planet',
+        description: 'Star wars univer',
+        imageUrl: 'https://images.unsplash.com/photo-1507499739999-097706ad8914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'espace'
+      },
+      {
+        title: 'BB-8',
+        description: 'Droïd',
+        imageUrl: 'https://images.unsplash.com/photo-1592513735373-ae19bac7fde8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        createdDate: new Date(),
+        snaps: 0
+      },
+    ];
   }
 }
