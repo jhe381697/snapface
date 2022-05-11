1a3197da78602ad17570f12074f37d84a7097e0d
jhe381697 <herlemont.josephine@gmail.com>
2022-05-12 00:41:26 +0200

    wip

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -12,7 +12,7 @@ export class FaceSnapsService {
           description: 'Sith',
           imageUrl: 'https://images.unsplash.com/photo-1623476408624-721c9185d569?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80',
           createdDate: new Date(),
-          snaps: 500,
+          snaps: 170,
           location: 'planète inconnue'
         },
         {
