3bcbb2c4f98aaadadb465324793a001730255ed3
jhe381697 <herlemont.josephine@gmail.com>
2022-05-14 18:45:26 +0200

    operateurs bas niveau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Observable } from 'rxjs';
+import { filter, interval, map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,20 @@ import { interval, Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
-  interval$!:  Observable<number>;
+  interval$!:  Observable<string>;
 
   ngOnInit() {
-    this.interval$ = interval(1000);
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ? 
+        `Je suis ${value} et je suis pair` :
+        `Je suis ${value} et je suis impair`
+      ),
+      tap(text => this.logger(text))
+    );
+  }
+
+  logger(text: string) {
+    console.log(`log: ${text}`);
   }
 }
